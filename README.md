f56bc78f42044e17c3d7a94ddf0e62132f9ba1f8
Your Name <you@example.com>
2021-07-04 23:25:52 +0530

    Initial build

---
+++ README.md
@@ -0,0 +1,6 @@
+#Download reels
+Hello , If you wanted to download instagram reels video then this library can help you.
+##Syntax:-
+-pip install reels
+-from reels import reels-downloader.download("url","Name.mp4")
+#Thats it Youre work has done..
---
+++ setup.py
@@ -0,0 +1,9 @@
+from  setuptools import setup
+setup(name="insta-reels",
+version="0.2",description="This can download instagram reels",author="@tanay_mishra",packages=['reels-downloader'],
+author_email="tanaymishra2204@gmail.com",install_requires=['requests-HTML'],
+long_description="""This module can download Instagram reels video 
+to download import download function from module and call with parameters 
+like that download('link','videos/video.mp4')..
+"""
+)
