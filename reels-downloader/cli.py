ef30e24c46d2dddcec1ffbefd8bfc60bd0ada7f7
tanaymishra <tanaymishra2204@gmail.com>
2021-07-31 11:40:04 +0530

    output flag for cli

--- reels-downloader/cli.py
+++ reels-downloader/cli.py
@@ -5,9 +5,11 @@ from reels import download, make_name
 def main():
     parser = argparse.ArgumentParser(description='download instagram reels')
     parser.add_argument('url', nargs='?')
+    parser.add_argument('-o', '--output')
     args = parser.parse_args()
     if args.url:
-        download(args.url, make_name(args.url))
+        name = args.output or make_name(args.url)
+        download(args.url, name)
 
 
 if __name__ == '__main__':
