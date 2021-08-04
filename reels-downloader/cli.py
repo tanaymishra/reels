fc9a7920d7f27a4f7a1d991338ff7a2e20944d19
tanaymishra <tanaymishra2204@gmail.com>
2021-08-04 14:45:26 +0530

    batch list flag

--- reels-downloader/cli.py
+++ reels-downloader/cli.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from reels import download, make_name
+from reels import download, make_name, download_from_file
 
 
 def main():
@@ -8,8 +8,11 @@ def main():
     parser.add_argument('url', nargs='?')
     parser.add_argument('-o', '--output')
     parser.add_argument('-f', '--folder', default='.')
+    parser.add_argument('-l', '--list')
     args = parser.parse_args()
-    if args.url:
+    if args.list:
+        download_from_file(args.list, args.folder)
+    elif args.url:
         name = args.output or make_name(args.url)
         download(args.url, os.path.join(args.folder, name))
 
