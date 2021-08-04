db53b0ad9be7635580a9883106a91c9def2a21ad
tanaymishra <tanaymishra2204@gmail.com>
2021-08-04 14:10:19 +0530

    folder flag for cli

--- reels-downloader/cli.py
+++ reels-downloader/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from reels import download, make_name
 
 
@@ -6,10 +7,11 @@ def main():
     parser = argparse.ArgumentParser(description='download instagram reels')
     parser.add_argument('url', nargs='?')
     parser.add_argument('-o', '--output')
+    parser.add_argument('-f', '--folder', default='.')
     args = parser.parse_args()
     if args.url:
         name = args.output or make_name(args.url)
-        download(args.url, name)
+        download(args.url, os.path.join(args.folder, name))
 
 
 if __name__ == '__main__':
